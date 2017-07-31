5f13318175528b9f056ac34e0954f67ecc5e9263
Zhou <363420113@qq.com>
2017-07-31 18:09:44 +0800

    更新reset.less

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,11 +1,10 @@
 /**
  * Created by ZZH on 2017/7/15.
  */
-require(['jquery','artTemplate','./app/util'], function ($,template,util) {
-    util.test();
-    console.log(util.url);
+require(['jquery','artTemplate','util'], function ($,template,util) {
     $.get('_page/index.html',function(data){
         var render = template.compile(data);
         $("#page").html(render(data));
     });
+    $.getTpl('_page/index');
 });
