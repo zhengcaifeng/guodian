020f5554fa2f5681260265c4b876baabdfc4a640
Zhou <363420113@qq.com>
2017-07-31 19:35:44 +0800

    添加弹窗

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,7 +1,8 @@
 /**
  * Created by ZZH on 2017/7/15.
  */
-require(['jquery','artTemplate','util'], function ($,template,util) {
+require(['jquery','artTemplate','util','layer'], function ($,template,util,layer) {
+    layer.alert('见到你真的很高兴', {icon: 2});
     $.get('_page/index.html',function(data){
         var render = template.compile(data);
         $("#page").html(render(data));
