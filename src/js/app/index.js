d5d4b77a2e9ab5f893e3bd1b421ae87185c52534
itadong <itadong@163.com>
2017-08-01 12:38:16 +0800

    zu

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -2,9 +2,13 @@
  * Created by ZZH on 2017/7/15.
  */
 require(['jquery','artTemplate','util'], function ($,template,util) {
-    $.get('_page/index.html',function(data){
-        var render = template.compile(data);
-        $("#page").html(render(data));
+    // $.get('_page/index.html',function(data){
+    //     var render = template.compile(data);
+    //     $("#page").html(render(data));
+    // });
+    $.getTpl({
+        type:1,
+        tpl:'title',
+        target:'#header'
     });
-    $.getTpl('_page/index');
 });
