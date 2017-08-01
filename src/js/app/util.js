6365dbb194658f63299550b78c929d29b4128210
luyouwei <2458578802@qq.com>
2017-08-01 09:20:53 +0800

    searchBar

--- src/js/app/util.js
+++ src/js/app/util.js
@@ -1,9 +1,58 @@
+/*
+Created by 周志豪 on 2017/7/31.
+公共js库，包含各种公共方法
+ */
+
 define(['jquery'],function($){
-    var fn = {
+    var g_data = {
+        debug:true,
         url:'http:127.1.1.1'
     };
-    fn.test = function(){
-        console.log('test');
-    };
-    return fn;
+    //全局ajax设置
+    $.ajaxSetup({
+        type:'POST',
+        async:true,
+        cache:false,
+        timeout:100000,
+        beforeSend:function(data){
+            console.log(data);
+        },
+        statusCode:{
+            404:function(){
+
+            },
+            401:function(){
+
+            }
+        },
+        fail:function(jqXHR, textStatus, errorThrown){
+            if('timeout' === textStatus){
+                alert('网络连接超时');
+            }else if('abort' === textStatus){
+
+            }else{
+                alert('网络异常');
+            }
+        }
+    });
+    //统一日志打印
+    $.fn['log'] = $['log'] = function (log) {
+        if(true === g_data.debug){
+            console.log(log);
+        }
+    }
+    //模板渲染方法
+    $.fn['getTpl'] = $['getTpl'] = function (options,callback) {
+        var url = '';
+        if(options.type === 1){
+            url = 'component/' + options.tpl + '.html';
+        }else if(options.type === 2){
+            url = '_page/' + options.tpl + '.html';
+        }
+        $.ajax({
+            type:'GET',
+            url:url
+        });
+    }
+    return g_data;
 });
