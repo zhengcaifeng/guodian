a8f3a4116864bf36106593c6bcd12b826a930e79
yaolina <yaolina@tydic.com>
2017-08-02 16:15:24 +0800

    修改对话框样式、供应商中心样式、采购商中心样式

--- src/js/app/gdLayerIndex.js
+++ src/js/app/gdLayerIndex.js
@@ -13,11 +13,39 @@ require(['jquery','layer','./app/util','./app/gdLayer'], function ($,layer,util)
             type = _this.data("fn-type");
         var lwidth = "";
         var lheight = "";
-        if(type == "base"){
+        var text = "";
+        var littleMsg = "";
+        if(type == "base" || type == "twoLayer"){
             lwidth = "522px";
             lheight = "290px";
         }
-        if(type == "warn" || type == "success" || type == "error" || type == "message" || type == "prompt"){
+        if(type == "warn"){
+            text = "这是进行一项操作时必须了解的重要信息";
+            littleMsg = "一些重要的需要用户知道的信息，你还要继续吗？";
+            lwidth = "390px";
+            lheight = "190px";
+        }
+        if(type == "success"){
+            text = "这是一条成功通知信息";
+            littleMsg = "一些附加信息一些附加信息一些附加信息";
+            lwidth = "390px";
+            lheight = "190px";
+        }
+        if(type == "error"){
+            text = "这是一条错误提示";
+            littleMsg = "一些附加信息一些附加信息一些附加信息";
+            lwidth = "390px";
+            lheight = "190px";
+        }
+        if(type == "message"){
+            text = "这是一条通知信息";
+            littleMsg = "一些附加信息一些附加信息一些附加信息";
+            lwidth = "390px";
+            lheight = "190px";
+        }
+        if(type == "prompt"){
+            text = "这是一个标题";
+            littleMsg = "此处是一条通知提醒，衬衫的价格是九磅十五便士，所以你选择B选项。 衬衫的价格是九磅十五便士，所以你选择B选项。";
             lwidth = "390px";
             lheight = "190px";
         }
@@ -25,13 +53,13 @@ require(['jquery','layer','./app/util','./app/gdLayer'], function ($,layer,util)
             "width":lwidth,
             "height": lheight,
             "type": type,
-            "text": "这是要显示的",
-            "littleMsg": "你看不到"
+            "text": text,
+            "littleMsg": littleMsg
         }, function(index) {
             layer.msg("正确", {
                 time: 2000
             });
-            layer.close(index);
+            //layer.close(index);
         });
     });
 });
