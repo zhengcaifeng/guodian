3031f40a484f039ca25e93e43420847c1185d385
itadong <itadong@163.com>
2017-08-05 09:14:20 +0800

    '更改文件名和参数名'

--- src/js/app/pagination.js
+++ src/js/app/pagination.js
@@ -4,7 +4,7 @@
 define(['jquery','laypage'], function ($,laypage) {
     var _laypage={
         paging:{
-            cont: 'page_default', //容器。值支持id名、原生dom对象，jquery对象,
+            ele: 'page_default', //容器。值支持id名、原生dom对象，jquery对象,
             pages: 10, //总页数
             skip: true, //是否开启跳页
             skin: 'gd',
