05559e8573bc78cfe1268826f7c083320abd5862
yaolina <yaolina@tydic.com>
2017-08-05 10:14:26 +0800

    修改首页、商城搜索框样式，弹框方法入参调整

--- src/js/app/dialog.js
+++ src/js/app/dialog.js
@@ -2,50 +2,66 @@ define(['jquery','layer'],function($,layer){
    var dialog = {};
     (function(){
         var fn = {};
+        /*普通弹框（模态框）
+        * 入参option包括：title(标题)，
+        *                content（显示的内容，文字或者html代码都可），
+        *                width（宽度），
+        *                height（高度，必填）
+        * callback方法：点击确定按钮执行的方法
+        * cancelCallBack方法：点击取消执行的方法
+        */
         dialog.modal = function(option, callback,cancelCallBack) {
-            fn.publicLayer_open(option["title"],option["content"],callback,cancelCallBack,1,option["width"],option["height"]);
+            fn.publicLayer_open(option,callback,cancelCallBack);
         };
 
+        /*信息框
+        * 入参option包括：type(信息类型，分为success,error,warn,message,prompt)，
+        *                data（包括text和littleMsg，分别是标题和后端返回的信息），
+        *                width（宽度），
+        *                height（高度，必填）
+         * callback方法：点击确定按钮执行的方法
+         * cancelCallBack方法：点击取消执行的方法
+        */
         dialog.alert = function(option, callback,cancelCallBack){
-            if(option["type"]) {
-                fn.publicLayer_msg(option["type"],option["data"], callback,cancelCallBack,option["width"],option["height"]);
-            }
+            fn.publicLayer_msg(option, callback,cancelCallBack);
         };
 
-        fn.publicLayer_msg = function(type,data,callback,cancelCallBack,width,height){
+        fn.publicLayer_msg = function(option,callback,cancelCallBack){
             var result = "";
             var icon = "";
             var typeBtn = null;
             var cssSkin = "";
             var lwidth = "";
-            width !="" ? lwidth = width : lwidth = "390";
-            switch(type){
+            option.width !="" && option.width !="auto"? lwidth = option.width+'px' : lwidth = "390px";
+            var lheight = "";
+            option.height !="" && option.height !="auto"? lheight = option.height+'px' : lheight = "auto";
+            switch(option.type){
                 case "prompt" :
-                    result = data.text + '<div style="padding-top: 10px;color: #999;font-size: 12px;line-height: 17px">' + data.littleMsg + '</div>'
+                    result = option.data.text + '<div style="padding-top: 10px;color: #999;font-size: 12px;line-height: 17px">' + option.data.littleMsg + '</div>'
                     icon = -1;
                     typeBtn = ['确定','取消'];
                     cssSkin = "prompt-layer";
                     break;
                 case "message" :
-                    result = data.text + '<div style="color: #999;font-size: 12px;">' + data.littleMsg + '</div>';
+                    result = option.data.text + '<div style="color: #999;font-size: 12px;">' + option.data.littleMsg + '</div>';
                     icon = 3;
                     typeBtn = ['知道了'];
                     cssSkin = "msg-Layer";
                     break;
                 case "warn" :
-                    result = data.text + '<div style="color: #999;font-size: 12px;">' + data.littleMsg + '</div>';
+                    result = option.data.text + '<div style="color: #999;font-size: 12px;">' + option.data.littleMsg + '</div>';
                     icon = 7;
                     typeBtn = ['确定','取消'];
                     cssSkin = "msg-Layer";
                     break;
                 case "success" :
-                    result = data.text + '<div style="color: #999;font-size: 12px;">' + data.littleMsg + '</div>';
+                    result = option.data.text + '<div style="color: #999;font-size: 12px;">' + option.data.littleMsg + '</div>';
                     icon = 1;
                     typeBtn = ['确定'];
                     cssSkin = "msg-Layer";
                     break;
                 case "error" :
-                    result = data.text + '<div style="color: #999;font-size: 12px;">' + data.littleMsg + '</div>';
+                    result = option.data.text + '<div style="color: #999;font-size: 12px;">' + option.data.littleMsg + '</div>';
                     icon = 2;
                     typeBtn = ['确定','取消'];
                     cssSkin = "msg-Layer";
@@ -57,7 +73,7 @@ define(['jquery','layer'],function($,layer){
                 skin: cssSkin,
                 time: 0,
                 shade : [0.5 , '#000' , true],
-                area: [lwidth+'px', height+'px'],
+                area: [lwidth, lheight],
                 btn: typeBtn,
                 yes: function(index) {
                     callback ? callback(index) : layer.close(index);
@@ -66,10 +82,10 @@ define(['jquery','layer'],function($,layer){
                     cancelCallBack ? cancelCallBack(index) : layer.close(index);
                 },
                 success:function(index){
-                    if(type == "prompt"){
+                    if(option.type == "prompt"){
                         $('.layui-layer-dialog .layui-layer-content').css("padding","20px 20px 20px 20px");
                     }
-                    if(type == "message" || type == "warn" || type == "success" || type == "error"){
+                    if(option.type == "message" || option.type == "warn" || option.type == "success" || option.type == "error"){
                         $('.layui-layer-dialog .layui-layer-content .layui-layer-ico').css("top","30px");
                         $('.layui-layer-dialog .layui-layer-content .layui-layer-ico').css("left","30px");
                         $('.layui-layer-dialog .layui-layer-padding').css("padding","30px 20px 10px 70px");
@@ -78,17 +94,19 @@ define(['jquery','layer'],function($,layer){
             });
         };
 
-        fn.publicLayer_open = function(layerTitle,resultText,callback,cancelCallBack,layerType,width,height){
+        fn.publicLayer_open = function(option,callback,cancelCallBack){
             var lwidth = "";
-            width !="" ? lwidth = width : lwidth = "522";
+            option.width !="" && option.width !="auto"? lwidth = option.width+'px' : lwidth = "522px";
+            var lheight = "";
+            option.height !="" && option.height !="auto"? lheight = option.height+'px' : lheight = "auto";
             layer.open({
                 type:1,
                 extend: 'gdLayer.css', //加载您的扩展样式
                 skin: 'base-Layer',
                 shade : [0.5 , '#000' , true],
-                area: [lwidth+"px", height+"px"],
-                title:layerTitle,
-                content: resultText,
+                area: [lwidth, lheight],
+                title:option.title,
+                content: option.content,
                 btn: ['确定', '取消'],
                 yes: function(index) {
                     callback ? callback(index) : layer.close(index);
